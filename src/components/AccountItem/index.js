e14b01c086146e2342d07dc4c7c5a90e3401350a
ta241003 <ngotheanh241003@gmail.com>
2023-07-02 21:04:03 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,14 +1,15 @@
 import classNames from "classnames/bind";
-import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Image from "~/components/Image";
+import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles)
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/42a81079b5885e152707b170d63ba2df~c5_100x100.jpeg?x-expires=1688184000&x-signature=Y2PykL%2FqOR9mYk8MLSLQZl7BB3w%3D" alt="Hoaa"/>
+            <Image className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/42a81079b5885e152707b170d63ba2df~c5_100x100.jpeg?x-expires=1688184000&x-signature=Y2PykL%2FqOR9mYk8MLSLQZl7BB3w%3D" alt="Hoaa"/>
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyen Van A</span>
