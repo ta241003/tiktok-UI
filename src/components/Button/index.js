4fee081bb80a6bcf9c58a3874167c73334338f92
ta241003 <ngotheanh241003@gmail.com>
2023-07-09 09:48:01 +0700

    test commit

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
@@ -19,38 +20,33 @@ function Button({
     leftIcon,
     rightIcon,
     onClick,
-     ...passProps
-
+    ...passProps
 }) {
     let Comp = 'button';
-
     const props = {
         onClick,
         ...passProps,
     };
 
+    // Remove event listener when btn is disabled
     if (disabled) {
-        // c1:
-        // delete props.onClick;
-
-        // c2:
         Object.keys(props).forEach((key) => {
-            if (key.startsWith('on') && typeof props[key] === 'function' ) {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
                 delete props[key];
             }
         });
     }
 
     if (to) {
-        props.to = to
-        Comp = Link
+        props.to = to;
+        Comp = Link;
     } else if (href) {
-        props.href = href
-        Comp = 'a'
+        props.href = href;
+        Comp = 'a';
     }
 
     const classes = cx('wrapper', {
-        [className] : className,
+        [className]: className,
         primary,
         outline,
         text,
@@ -68,5 +64,22 @@ function Button({
         </Comp>
     );
 }
+
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    rounded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
 
 export default Button;
